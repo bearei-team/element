7536e92d986d876e9d9ec8ad2f468db2be21326b
bearei <bear_ei@outlook.com>
2023-10-09 11:33:28 +0800

    chore: initialize the project

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: false,
+    singleQuote: true,
+    trailingComma: 'all',
+};
